3977752f3cfb55bfc1de20d43517e90ebe7f4ca4
Ahmed Abou-Zeid <ahmedmaika@gmail.com>
2022-02-08 18:45:31 +0200

    make home view model implementation

--- OrangeNewsTask/ViewModel/HomeViewModel.swift
+++ OrangeNewsTask/ViewModel/HomeViewModel.swift
@@ -6,7 +6,17 @@
 //
 
 import Foundation
+import RxSwift
 
 class HomeViewModel {
+    let clientService: ClientServiceProtocol
+    
+    init(clientService: ClientServiceProtocol = ClientService()) {
+        self.clientService = clientService
+    }
+    
+    func getHeadlinesList(country: String, category: String) -> Observable<[CategoryData]> {
+        return clientService.getNewsList(country: country, category: category)
+    }
     
 }
